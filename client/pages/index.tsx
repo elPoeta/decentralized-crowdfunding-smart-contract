73c0eb8a6c90ec7be83ce2982acdec9ade1e65fd
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-06-27 19:02:42 -0300

    create next app with  tailwind

---
+++ client/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Crowdfund</title>
+        <meta name="description" content="Decentralized crowdfunding smart contract" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+      Crowdfund
+      </main>
+    </>
+  )
+}
+
+export default Home
